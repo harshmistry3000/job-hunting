e7ae087289195c36fdf9089092057c7919f7586b
harshmistry3000 <harshmistry3000@gmail.com>
2024-11-26 00:08:16 +0530

    register controller done

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,16 +1,34 @@
-export const register  = async(req,res)=> {
-    try{
-        const {fullname,email,phoneNumber,password,role} = req.body;
+import { User } from "../models/user.models";
+import bcrypt from "bcryptjs";
+
+export const register = async (req, res) => {
+    try {
+        const { fullname, email, phoneNumber, password, role } = req.body;
         if (!fullname || !email || !phoneNumber || !password || !role) {
             return res.status(400).json({
-                message:"Something is missing",
-                success:false                
+                message: "Something is missing",
+                success: false
             });
-            
+
         };
-        
-    }
-    catch (error){
-        
-    }
+        const user = await User.findOne({email});
+        if(user){
+            return res.status(400).json({
+                message:'User already exist with this email.',
+                success:false,
+            })
+        }
+            const hashedPassword = await bcrypt.hash(password,10);
+            
+            await  User.create({
+                fullname, 
+                email, 
+                phoneNumber, 
+                password:hashedPassword, 
+                role,
+            })
+
+    } catch (error) {
+
+  }
 }
