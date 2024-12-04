3c7503b71e7f8105f63364d1c93fe0e02991f329
harshmistry3000 <harshmistry3000@gmail.com>
2024-12-04 16:19:16 +0530

    Navbar done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/shared/Navbar'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <h1>hello </h1>
+  <Navbar/>    
+
     </>
   )
 }
---
+++ frontend/src/components/shared/Navbar.jsx
@@ -0,0 +1,79 @@
+import { Popover, PopoverTrigger } from '@radix-ui/react-popover'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { PopoverContent } from '../ui/popover'
+import { Avatar, AvatarImage } from '../ui/avatar'
+import { Button } from '../ui/button'
+import { LogIn, LogOut, User2 } from 'lucide-react'
+
+const Navbar = () => {
+
+    const user = false;
+    return (
+        <div className='bg-white'>
+
+            <div className='flex items-center justify-between mx-auto max-w-7xl h-16'>
+                <div>
+                    <h1 className='text-4xl text-[#003566] font-bold' >JOB <span className='text-[#FFC300]'>HUNT</span> </h1>
+                </div>
+                <div className='flex items-center gap-16'>
+                    <ul className='flex font-medium items-center gap-5'>
+                        <li>Home</li>
+                        <li>Job</li>
+                        <li>Browse</li>
+                    </ul>
+
+
+                    {
+                        !user ? (
+                            <div className='flex items-center gap-3'> 
+                                <Button  variant="secondary" className="bg-[#ffe45eb0] hover:bg-[#FFD60A]"> Login</Button>
+                                <Button variant="" className="bg-[#003566] hover:bg-[#001D3D]" >Signup </Button>
+                            </div>
+                        ) : (
+                            <Popover>
+                                <PopoverTrigger asChild>
+
+                                    <Avatar className="cursor-pointer">
+                                        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                                    </Avatar>
+
+                                </PopoverTrigger>
+                                <PopoverContent className="w-80">
+
+                                    <div className=''>
+                                        <Avatar className="cursor-pointer">
+                                            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                                        </Avatar>
+                                        <div>
+                                            <h4 className='font-medium'> peter parker</h4>
+                                            <p>spider man</p>
+                                        </div>
+                                        <div className='flex flex-col my-2 gap-1 text-[#118ab2]'>
+                                            <div className='flex w-fit items-center gap-2 cursor-pointer'>
+
+                                                <User2 /> <
+                                                    Button variant="ghost">View Profile</Button>
+                                            </div>
+                                            <div className='flex w-fit items-center gap-2 cursor-pointer'>
+                                                <LogOut />
+                                                <Button variant="ghost">Logout</Button>
+                                            </div>
+                                        </div>
+
+                                    </div>
+
+
+
+                                </PopoverContent>
+                            </Popover>
+                        )
+                    }
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
